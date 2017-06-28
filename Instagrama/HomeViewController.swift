18b084d771741020ad77c72e4b56e898fc33c2f9
Marissa Bush <marissanbush1@verizon.net>
2017-06-28 11:58:51 -0700

    camera and photo galleryadded on one tab and added a tab controller

--- Instagrama/HomeViewController.swift
+++ Instagrama/HomeViewController.swift
@@ -7,22 +7,38 @@
 //
 
 import UIKit
+import Parse
 
 class HomeViewController: UIViewController {
 
+    @IBAction func logOutButton(_ sender: Any) {
+        
+        PFUser.logOutInBackground { (error: Error?) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+            } else{
+                print("Log out successful")
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let LoginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+                self.present(LoginViewController, animated: true, completion: {
+                })
+            }
+                
+            // PFUser.currentUser() will now be nil
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
+        /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
---
+++ Instagrama/PhotoViewController.swift
@@ -0,0 +1,57 @@
+//
+//  PhotoViewController.swift
+//  Instagrama
+//
+//  Created by Marissa Bush on 6/28/17.
+//  Copyright © 2017 Marissa Bush. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
